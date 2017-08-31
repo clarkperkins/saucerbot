40e7ae6d1dd36bfb2bc43e7b7c5d38f9280fd164
Clark Perkins <clark.perkins@digitalreasoning.com>
2017-08-30 20:07:48 -0500

    only send reminders on monday

--- saucerbot/commands.py
+++ saucerbot/commands.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+
+import click
 from saucerbot import app
 from saucerbot.utils import send_message
 
 
 @app.cli.command()
-def remind():
+@click.option('--force', is_flag=True, help='Forces saucerbot to send a reminder on non-mondays')
+def remind(force):
     """
     Remind everyone to come to saucer
     """
-    send_message('Saucer at 7PM. Like if.')
+    today = datetime.datetime.now()
+
+    if today.weekday() == 0 or force:
+        send_message('Saucer at 7PM. Like if.')
+    else:
+        click.echo("Not sending message, it's not Monday!")
