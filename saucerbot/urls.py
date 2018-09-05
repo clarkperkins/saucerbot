4bb42865cd3769d06d914873f6c8c774a41e4d41
Clark Perkins <clark.perkins@digitalreasoning.com>
2018-09-05 13:56:15 -0500

    Initial channels changes

--- saucerbot/urls.py
+++ saucerbot/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('groupme/', include('saucerbot.groupme.urls')),
+    path('groupme/', include('saucerbot.groupme.urls', namespace='groupme')),
     path('admin/', admin.site.urls),
 ]
