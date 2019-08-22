8c57ed587598a213bc4b6776690d16261c596a0d
Clark Perkins <r.clark.perkins@gmail.com>
2019-08-22 08:37:32 -0500

    Updating files for django 2.2

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saucerbot.settings")
-    os.environ.setdefault("DJANGO_ENV", "development")
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saucerbot.settings')
+    os.environ.setdefault('DJANGO_ENV', 'development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -14,3 +16,7 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
