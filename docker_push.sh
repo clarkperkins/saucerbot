72e3dcbf1c073da49a54d5905f89a2bd3f269f0e
Clark Perkins <r.clark.perkins@gmail.com>
2020-12-17 22:22:50 -0600

    Push docker image for branches

--- docker_push.sh
+++ docker_push.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" == "main" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    make build
-
-    echo "Pushing to docker hub"
-    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-    docker push clarkperkins/saucerbot
+if [ ! -z "TRAVIS_PULL_REQUEST_BRANCH" ]; then
+  git_branch=$TRAVIS_PULL_REQUEST_BRANCH
+  git_status=""
+elif [ ! -z "$TRAVIS_BRANCH" ]; then
+  git_branch=$TRAVIS_BRANCH
+  git_status=""
 else
-    echo "Not deploying image"
+  git_branch=$(git branch --show-current)
+  git_status=$(git status --porcelain)
 fi
+
+make build
+
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+
+if [ "$git_branch" == "main" ] && [ -z "$git_status" ]; then
+    echo "Pushing docker image clarkperkins/saucerbot"
+    docker push clarkperkins/saucerbot
+fi
+
+image="clarkperkins/saucerbot:$git_branch"
+
+echo "Pushing docker image $image"
+
+docker tag clarkperkins/saucerbot $image
+docker push $image
