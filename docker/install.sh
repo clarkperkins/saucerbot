c33b98252dc579f682008d175b7aad9f1875d6ed
Clark Perkins <r.clark.perkins@gmail.com>
2021-11-03 10:59:20 -0500

    Build docker image during github workflow

--- docker/install.sh
+++ docker/install.sh
@@ -4,6 +4,8 @@ set -e
 
 export PACKAGES='curl'
 export BUILD_PACKAGES='gcc g++ git gnupg'
+export PG_PACKAGES='postgresql-client-14'
+export PG_BUILD_PACKAGES='libpq-dev'
 
 echo "Installing system dependencies..."
 apt-get update
@@ -14,18 +16,18 @@ echo "Installing postgresql-client..."
 echo "deb http://apt.postgresql.org/pub/repos/apt/ ${VERSION_CODENAME}-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 curl -sS https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update
-apt-get install -y --no-install-recommends postgresql-client-12
+apt-get install -y --no-install-recommends $PG_PACKAGES $PG_BUILD_PACKAGES
 
 echo "Installing pipenv..."
-pip install pipenv
+python -m pip install pipenv
 
 echo "Installing python dependencies..."
 pipenv install --deploy --system
 
 echo "Removing pipenv..."
-pip uninstall -y appdirs distlib filelock pipenv virtualenv virtualenv-clone
+python -m pip uninstall -y backports.entry-points-selectable distlib filelock pipenv platformdirs virtualenv virtualenv-clone
 
 echo "Removing unneeded system dependencies..."
-apt-get remove -y $BUILD_PACKAGES
+apt-get remove -y $BUILD_PACKAGES $PG_BUILD_PACKAGES
 apt-get -y autoremove
 rm -rf /var/lib/apt/lists/*
